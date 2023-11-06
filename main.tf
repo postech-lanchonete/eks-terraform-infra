2f3dad5dad5f733885776856976e4d2a0cc5b1d3
Silva Daniel, SUB-SDC-NL-MBM-BNC <Daniel.Silva@swisscom.com>
2023-11-06 10:17:30 +0100

    mudanca do nome do keypar

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ module "eks" {
   scaling_max_size        = 1
   scaling_min_size        = 1
   instance_types          = ["t2.micro"]
-  key_pair                = "TestKeyPair"
+  key_pair                = "ec2-micro"
 }
 
 module "vpc" {
