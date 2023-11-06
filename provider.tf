3ce09484aa094153cda85fabfd89dac6546cace9
Silva Daniel, SUB-SDC-NL-MBM-BNC <Daniel.Silva@swisscom.com>
2023-11-06 17:10:39 +0100

    max-size

--- provider.tf
+++ provider.tf
@@ -10,13 +10,14 @@ terraform {
       source  = "hashicorp/random"
       version = "3.1.0"
     }
+    
     kubernetes = {
       source  = "hashicorp/kubernetes"
       version = ">= 2.0.1"
     }
   }
 
-backend "remote" {
+  backend "remote" {
 		hostname = "app.terraform.io"
 		organization = "lanchonetebairro"
 
