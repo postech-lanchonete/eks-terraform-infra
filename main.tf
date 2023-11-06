a96a60cf298e2ae7617a020271ee8d65b6b44ff0
Daniel Maria da Silva <danielmariadasilva@gmail.com>
2023-11-06 17:17:22 +0100

    muda todo o scaling para 2

--- main.tf
+++ main.tf
@@ -8,9 +8,9 @@ module "eks" {
   endpoint_private_access = false
   public_access_cidrs     = ["0.0.0.0/0"]
   node_group_name         = "lanchonetebairro"
-  scaling_desired_size    = 1
+  scaling_desired_size    = 2
   scaling_max_size        = 2
-  scaling_min_size        = 1
+  scaling_min_size        = 2
   instance_types          = ["t2.micro"]
   key_pair                = "ec2-micro"
 }
